f633cf0044b88141c1188b8ceaf200589a3f208b
domingos <mds.junior87@gmail.com>
2024-06-15 13:05:33 -0300

    correção classe contato

---
+++ src/Contato.java
@@ -0,0 +1,17 @@
+public class Contato {
+    private String nome;
+    private String numeroTelefone;
+
+    public Contato(String nome, String numeroTelefone) {
+        this.nome = nome;
+        this.numeroTelefone = numeroTelefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getNumeroTelefone() {
+        return numeroTelefone;
+    }
+}
